77e489735b09c2d5fa1545f333b85f7fa4bdf766
truong <truongnguyen30999@gmail.com>
2020-06-26 00:26:41 +0700

    Fix small bugs

--- do_an_web/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ do_an_web/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using do_an_web.Extensions;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using do_an_web.Extensions;
 
 namespace do_an_web.Areas.Identity.Pages.Account
 {
@@ -30,6 +32,9 @@ namespace do_an_web.Areas.Identity.Pages.Account
         {
             await _signInManager.SignOutAsync();
             _logger.LogInformation("User logged out.");
+            List<int> lstCartItems = new List<int>();
+            HttpContext.Session.Set("ssShoppingCart", lstCartItems);
+
             if (returnUrl != null)
             {
                 return LocalRedirect(returnUrl);
